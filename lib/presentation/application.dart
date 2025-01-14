4b27d79ab0605526cc65f9740750b739ec431be7
ViktorDov <dovhinka.dev@gmail.com>
2025-01-14 19:17:50 +0200

    main

--- lib/presentation/application.dart
+++ lib/presentation/application.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: const HomeScreen(),
     );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -10,6 +10,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const HomeScreenBody();
+  }
+}
+
+class HomeScreenBody extends StatelessWidget {
+  const HomeScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Smart Tailor'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: const HomeButtonFormWidget(),
+      ),
+    );
+  }
+}
+
+class HomeButtonFormWidget extends StatelessWidget {
+  const HomeButtonFormWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/login');
+          },
+          child: const Text('Login'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/register');
+          },
+          child: const Text('Register'),
+        ),
+      ],
+    );
   }
 }
